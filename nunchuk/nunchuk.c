ae30a4c44347807d0c13429762c9968b77fd5fab
Marcin Hyzorek <m.hyzorek@samsung.com>
2023-11-24 11:34:12 +0100

    automated quick commit

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -10,18 +10,24 @@
 #include <linux/i2c.h>
 #include <linux/input.h>
 
+#define N_INIT_BYTES 2
 #define INIT_BYTES_1 {0xf0, 0x55}
 #define INIT_BYTES_2 {0xfb, 0x00}
-#define N_INIT_BYTES 2
+
+#define N_INIT_READ_BYTES 1
+#define N_BYTES_TO_READ 6
+#define BUTTON_STATE_INDEX 5
 #define INIT_READ_BYTES {0x00}
+
 #define DEV_NAME "nunchuk"
+#define DELAY 10000
+#define DELAY_MAX 20000
+#define POLL_INTERVAL 50
 
 #define PRESSED 1
 #define RELEASED 0
 
-#define POLL_INTERVAL 50
-
-static char read_buf[6];
+static char rx_buf[6];
 static int z_state, c_state;
 
 struct nunchuk_dev {
@@ -32,20 +38,19 @@ struct nunchuk_dev {
 int _nun_read_regs(struct i2c_client *client)
 {
 	int ret_val;
-	char tx[] = INIT_READ_BYTES;
+	char tx_buf[] = INIT_READ_BYTES;
 
-	usleep_range(10000, 20000);
+	usleep_range(DELAY, DELAY_MAX);
 
-	ret_val = i2c_master_send(client, tx, sizeof(tx));
-	if (ret_val != N_INIT_BYTES) {
-		pr_alert(DEV_NAME ": error %d while sending bytes starting with %c\n",
-				ret_val, tx[0]);
+	ret_val = i2c_master_send(client, tx_buf, sizeof(tx_buf));
+	if (ret_val != N_INIT_READ_BYTES) {
+		pr_alert(DEV_NAME ":i2c_master_send: error %d\n", ret_val);
 		return ret_val;
 	}
-	usleep_range(10000, 20000);
+	usleep_range(DELAY, DELAY_MAX);
 
-	ret_val = i2c_master_recv(client, read_buf, 6);
-	if (ret_val != N_INIT_BYTES) {
+	ret_val = i2c_master_recv(client, rx_buf, N_BYTES_TO_READ);
+	if (ret_val != N_BYTES_TO_READ) {
 		pr_alert(DEV_NAME ": i2c_master_recv: error %d\n", ret_val);
 		return ret_val;
 	}
@@ -62,10 +67,10 @@ void nun_poll(struct input_dev *input)
 	struct i2c_client *client = nun->i2c_client;
 
 	ret_val = _nun_read_regs(client);
-	if (ret_val < 0)
+	if (ret_val != N_BYTES_TO_READ)
 		return;
 
-	button_state_byte = read_buf[5];
+	button_state_byte = rx_buf[BUTTON_STATE_INDEX];
 	z_state = button_state_byte & 0b01 ? RELEASED : PRESSED;
 	c_state = button_state_byte & 0b10 ? RELEASED : PRESSED;
 
@@ -78,27 +83,27 @@ void nun_poll(struct input_dev *input)
 int nun_probe(struct i2c_client *client)
 {
 	int ret_val;
-	const char rx1[2] = INIT_BYTES_1;
-	const char rx2[2] = INIT_BYTES_2;
+	const char tx_buf1[2] = INIT_BYTES_1;
+	const char tx_buf2[2] = INIT_BYTES_2;
 
 	struct input_dev *input;
 	struct nunchuk_dev *nun;
 
 
 	/* Initialization */
-	ret_val = i2c_master_send(client, rx1, sizeof(rx1));
-	if (ret_val < 0) {
+	ret_val = i2c_master_send(client, tx_buf1, sizeof(tx_buf1));
+	if (ret_val != N_INIT_BYTES) {
 		pr_alert("error %d while sending bytes starting with %c\n",
-				ret_val, rx1[0]);
+				ret_val, tx_buf1[0]);
 		return ret_val;
 	}
 
-	udelay(1000);
+	udelay(DELAY);
 
-	ret_val = i2c_master_send(client, rx2, sizeof(rx2));
-	if (ret_val < 0) {
+	ret_val = i2c_master_send(client, tx_buf2, sizeof(tx_buf2));
+	if (ret_val != N_INIT_BYTES) {
 		pr_alert("error %d while sending bytes starting with %c\n",
-				ret_val, rx2[0]);
+				ret_val, tx_buf2[0]);
 		return ret_val;
 	}
 
@@ -111,6 +116,8 @@ int nun_probe(struct i2c_client *client)
 	nun =  devm_kzalloc(&client->dev, sizeof(*nun), GFP_KERNEL);
 	if (!nun)
 		return -ENOMEM;
+
+	/* Input subsystem registration */
 	nun->i2c_client = client;
 	input_set_drvdata(input, nun);
 
@@ -145,11 +152,11 @@ int nun_remove(struct i2c_client *client)
 }
 
 static const struct i2c_device_id nunchuk_id[] = {
-	{DEV_NAME},
+	{ DEV_NAME },
 	{  }
 };
 
-
+MODULE_DEVICE_TABLE(i2c, nunchuk_id);
 
 static const struct of_device_id nunchuk_of_match[] = {
 	{ .compatible = "nintendo," DEV_NAME },
@@ -167,8 +174,6 @@ struct i2c_driver nun_driver = {
 };
 
 
-
-MODULE_DEVICE_TABLE(i2c, nunchuk_id);
 module_i2c_driver(nun_driver);
 
 MODULE_LICENSE("GPL");

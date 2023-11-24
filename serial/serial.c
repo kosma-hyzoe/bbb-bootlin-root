b8d86a37e7e63834465009a1aa18476720abef05
Marcin Hyzorek <m.hyzorek@samsung.com>
2023-11-24 12:41:27 +0100

    automated quick commit

--- serial/serial.c
+++ serial/serial.c
@@ -5,9 +5,24 @@
 #include "linux/mod_devicetable.h"
 /* Add your code here */
 
+struct serial_dev {
+	void __iomem *regs;
+};
+
 static int serial_probe(struct platform_device *pdev)
 {
+	struct serial_dev *serial;
+
 	pr_info("Called %s\n", __func__);
+
+	serial = devm_kzalloc(&pdev->dev, sizeof(*serial), GFP_KERNEL);
+	if (!serial)
+		return -ENOMEM;
+
+	serial->regs = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(serial->regs))
+		return PTR_ERR(serial->regs);
+
 	return 0;
 }
 

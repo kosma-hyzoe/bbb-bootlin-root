0ad627e1ecbb9d082d150c2c873012a8199a924a
Marcin Hyzorek <m.hyzorek@samsung.com>
2023-11-24 12:32:12 +0100

    automated quick commit

--- serial/serial.c
+++ serial/serial.c
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: GPL-2.0
+#include "linux/device.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
 /* Add your code here */
 
+struct serial_dev {
+	void __iomem *regs;
+};
+
 static int serial_probe(struct platform_device *pdev)
 {
+	struct serial_dev *serial;
+
 	pr_info("Called %s\n", __func__);
+
+	serial = devm_kzalloc(&pdev->dev, sizeof(*serial), GFP_KERNEL);
+	if (!serial)
+		return -ENOMEM;
+
+	serial->regs = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(serial->regs))
+		return PTR_ERR(serial->regs);
+
 	return 0;
 }
 

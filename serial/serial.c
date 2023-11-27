c3c4a9c610b1b8ff8ffb1d1fcea20fcb8fc740f2
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-11-27 12:38:41 +0100

    stole a working serial.c as a reference

--- serial/serial.c
+++ serial/serial.c
@@ -1,22 +1,72 @@
 // SPDX-License-Identifier: GPL-2.0
+#include "linux/limits.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include "linux/mod_devicetable.h"
-#include "linux/serial_reg.h"
+
+#include <linux/of.h>
+#include <linux/pm_runtime.h>
+#include <linux/io.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/interrupt.h>
+#include <linux/wait.h>
+#include <linux/spinlock.h>
+#include <linux/sched.h>
+#include <uapi/linux/serial_reg.h>
+#include <linux/debugfs.h>
 /* Add your code here */
 
 struct serial_dev {
 	void __iomem *regs;
 };
 
-void reg_write(struct serial_dev serial, u32 val, int reg)
+static u32 reg_read(struct serial_dev *serial, unsigned int reg)
+{
+	return *(u32 *)(serial->regs + (reg << 2));
+}
+
+static void reg_write(struct serial_dev *serial, u32 val, unsigned int reg)
 {
 
+	/* writel(val, serial->regs + (reg << 2)); */
+	*(u32 *)(serial->regs + (reg << 2)) = val;
+}
+
+static int _config_baud_rate(struct serial_dev *serial,
+		struct platform_device *pdev)
+{
+	int ret;
+	unsigned int baud_divisor, uartclk;
+	ret = of_property_read_u32(pdev->dev.of_node, "clock-frequency",
+			&uartclk);
+	if (ret <  0) {
+		dev_err(&pdev->dev,
+			"clock-frequency property not found in Device Tree\n");
+		return ret;
+	}
+	baud_divisor = uartclk / 16 / 115200;
+	reg_write(serial, 0x07, UART_OMAP_MDR1);
+	reg_write(serial, 0x00, UART_LCR);
+	reg_write(serial, UART_LCR_DLAB, UART_LCR);
+	reg_write(serial, baud_divisor & 0xff, UART_DLL);
+	reg_write(serial, (baud_divisor >> 8) & 0xff, UART_DLM);
+	reg_write(serial, UART_LCR_WLEN8, UART_LCR);
+
+	return 0;
+}
+
+static void serial_write_char(struct serial_dev *dev, u8 val)
+{
+	while ((reg_read(dev, UART_LSR) & UART_LSR_THRE) == 0)
+		cpu_relax();
+
+	reg_write(dev, val, UART_TX);
 }
 
 static int serial_probe(struct platform_device *pdev)
 {
+	int ret_val;
 	struct serial_dev *serial;
 
 	pr_info("Called %s\n", __func__);
@@ -28,14 +78,29 @@ static int serial_probe(struct platform_device *pdev)
 	serial->regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(serial->regs))
 		return PTR_ERR(serial->regs);
+
+	pm_runtime_enable(&pdev->dev);
+	pm_runtime_get_sync(&pdev->dev);
+
+	ret_val = _config_baud_rate(serial, pdev);
+	if (ret_val < 0) {
+		pm_runtime_disable(&pdev->dev);
+		return ret_val;
+	}
+
+
 	reg_write(serial, UART_FCR_CLEAR_RCVR | UART_FCR_CLEAR_XMIT, UART_FCR);
+	reg_write(serial, 0x00, UART_OMAP_MDR1);
+	serial_write_char(serial, 'f');
+
 	return 0;
 }
 
 static int serial_remove(struct platform_device *pdev)
 {
 	pr_info("Called %s\n", __func__);
-        return 0;
+	pm_runtime_disable(&pdev->dev);
+	return 0;
 }
 
 
@@ -47,13 +112,13 @@ static const struct of_device_id serial_of_match[] = {
 MODULE_DEVICE_TABLE(of, serial_of_match);
 
 static struct platform_driver serial_driver = {
-        .driver = {
-                .name = "bootlin-serial",
-                .owner = THIS_MODULE,
+	.driver = {
+		.name = "bootlin-serial",
+		.owner = THIS_MODULE,
 		.of_match_table = serial_of_match,
-        },
-        .probe = serial_probe,
-        .remove = serial_remove,
+	},
+	.probe = serial_probe,
+	.remove = serial_remove,
 };
 module_platform_driver(serial_driver);
 MODULE_LICENSE("GPL");

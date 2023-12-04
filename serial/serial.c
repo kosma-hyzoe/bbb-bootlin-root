58952ec43b1b65268bcaf983bb26b8e40189e3ce
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-12-04 22:34:40 +0100

    automated quick commit

--- serial/serial.c
+++ serial/serial.c
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
+#include "asm-generic/errno-base.h"
+#include "linux/device/driver.h"
 #include "linux/limits.h"
+#include "linux/moduleparam.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -16,6 +19,8 @@
 #include <uapi/linux/serial_reg.h>
 #include <linux/debugfs.h>
 /* Add your code here */
+
+#define MAX_LINE_LEN 256
 
 struct serial_dev {
 	void __iomem *regs;
@@ -58,27 +63,57 @@ static int _config_baud_rate(struct serial_dev *serial,
 	return 0;
 }
 
-static void serial_write_char(struct serial_dev *dev, u8 val)
+static void serial_write_char(struct serial_dev *serial, u8 val)
 {
-	while ((reg_read(dev, UART_LSR) & UART_LSR_THRE) == 0)
+	while ((reg_read(serial, UART_LSR) & UART_LSR_THRE) == 0)
 		cpu_relax();
 
-	reg_write(dev, val, UART_TX);
+	reg_write(serial, val, UART_TX);
+	if (val == '\n')
+		serial_write_char(serial, '\r');
 }
 
 
-ssize_t serial_write(sturct file *f, const char __user *buf, size_t sz, loff_t *off)
+ssize_t serial_write(struct file *file, const char __user *buf, size_t sz, loff_t *off)
 {
+	struct miscdevice *miscdev_ptr = file->private_data;
+	struct serial_dev *serial = container_of(miscdev_ptr, struct serial_dev,
+			miscdev);
+	u8 c;
+	int i;
+	for (i = 0; i < sz; i++) {
+		if (get_user(c, buf + i))
+			return -EFAULT;
 
+		serial_write_char(serial, c);
+	}
+	*off += sz;
+
+	return sz;
 }
+
+ssize_t serial_read(struct file *f, char __user *buf, size_t sz, loff_t *off)
+{
+	return -1;
+}
+
+
+static const struct file_operations serial_fops = {
+	.owner = THIS_MODULE,
+	.read = serial_read,
+	.write = serial_write,
+	/* .unlocked_ioctl = serial_ioctl, */
+};
 
 static int serial_probe(struct platform_device *pdev)
 {
 	int ret_val;
 	struct serial_dev *serial;
+	struct resource *res;
 
 	pr_info("Called %s\n", __func__);
 
+	/* allocation and pointer madness */
 	serial = devm_kzalloc(&pdev->dev, sizeof(*serial), GFP_KERNEL);
 	if (!serial)
 		return -ENOMEM;
@@ -88,7 +123,6 @@ static int serial_probe(struct platform_device *pdev)
 	serial->regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(serial->regs))
 		return PTR_ERR(serial->regs);
-
 
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_get_sync(&pdev->dev);
@@ -104,14 +138,31 @@ static int serial_probe(struct platform_device *pdev)
 	reg_write(serial, UART_FCR_CLEAR_RCVR | UART_FCR_CLEAR_XMIT, UART_FCR);
 	reg_write(serial, 0x00, UART_OMAP_MDR1);
 
-	serial_write_char(serial, 'f');
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		return -1;
+	}
+	serial->miscdev.name = devm_kasprintf(&pdev->dev, GFP_KERNEL,
+			"serial-%x", res->start);
+	serial->miscdev.fops = &serial_fops;
+	serial->miscdev.parent = &pdev->dev;
+	serial->miscdev.minor = MISC_DYNAMIC_MINOR;
+	misc_register(&serial->miscdev);
 
 	return 0;
 }
 
 static int serial_remove(struct platform_device *pdev)
 {
+	struct serial_dev *dev = platform_get_drvdata(pdev);
+
 	pr_info("Called %s\n", __func__);
+
+	/* debugfs_remove(dev->debugfs_dir); */
+	misc_deregister(&dev->miscdev);
+	pm_runtime_disable(&pdev->dev);
+
+	dev_info(&pdev->dev, "remove complete\n");
 	pm_runtime_disable(&pdev->dev);
 	return 0;
 }
